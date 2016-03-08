d6a9574ac0dd7086a656b999c10708ca4cc7ea27
Herbert Wolverson <herberticus@gmail.com>
2016-03-08 09:57:39 -0600

    Window moved to a unique_ptr

--- rltk/rltk.cpp
+++ rltk/rltk.cpp
@@ -1,26 +1,36 @@
 #include "rltk.hpp"
 #include <SFML/Graphics.hpp>
+#include <memory>
 
 namespace rltk {
 
+std::unique_ptr<sf::RenderWindow> main_window;
+
 void run(std::function<void(double)> on_tick, const int window_width, const int window_height, const std::string window_title) {
-    sf::RenderWindow window(sf::VideoMode(window_width, window_height, sf::Style::Titlebar | sf::Style::Resize | sf::Style::Close), window_title);
+
+	std::make_unique<sf::RenderWindow>(sf::VideoMode(window_width, window_height, sf::Style::Titlebar | sf::Style::Resize | sf::Style::Close), window_title);
+
+    main_window->setVerticalSyncEnabled(true);
 
     double duration_ms = 0.0;
-    while (window.isOpen())
+    while (main_window->isOpen())
     {
     	clock_t start_time = clock();
 
         sf::Event event;
-        while (window.pollEvent(event))
+        while (main_window->pollEvent(event))
         {
-            if (event.type == sf::Event::Closed)
-                window.close();
+            if (event.type == sf::Event::Closed) {
+                main_window->close();
+            }
         }
 
-        window.clear();
+        main_window->clear();
+        sf::Vector2u size_pixels = main_window->getSize();
+
         on_tick(duration_ms);
-        window.display();
+
+        main_window->display();
 
         duration_ms = ((clock() - start_time) * 1000.0) / CLOCKS_PER_SEC;
     }
