f99ca4f2e3ae8199a98219a1f5589b77d14c574a
Herbert Wolverson <herberticus@gmail.com>
2016-03-08 09:25:50 -0600

    Obligatory demo moved into library folder.

--- examples/ex1/main.cpp
+++ examples/ex1/main.cpp
@@ -1,24 +1,8 @@
-#include <SFML/Graphics.hpp>
+#include "../../rltk/rltk.hpp"
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
-
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
+	rltk::run();
 
     return 0;
 }
