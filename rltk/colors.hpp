865f3a61deb436606b628a8b7db3e038d9d5c5f9
Herbert Wolverson <herbert@iMac.local>
2016-03-08 13:01:33 -0600

    A few more named colors.

--- rltk/colors.hpp
+++ rltk/colors.hpp
@@ -8,5 +8,7 @@ constexpr color_t BLACK{0,0,0};
 constexpr color_t WHITE{255,255,255};
 constexpr color_t YELLOW{255,255,0};
 constexpr color_t BLUE{0,0,255};
+constexpr color_t RED{255,0,0};
+constexpr color_t GREEN{0,255,0};
 
 }
