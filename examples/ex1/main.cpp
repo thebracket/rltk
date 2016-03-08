35e842efbc6f353a77126e498cfd8bfcf3cf4de9
Herbert Wolverson <herberticus@gmail.com>
2016-03-08 09:19:54 -0600

    The obligatory SFML works example.

--- examples/ex1/main.cpp
+++ examples/ex1/main.cpp
@@ -1,6 +1,24 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
 
-int main() {
-	std::cout << "Hello World\n";
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
 }
-
