fe06ac0ea9255ab8430aaf9fae055482ebc59251
Herbert Wolverson <herbert@iMac.local>
2016-03-08 09:25:50 -0600

    Obligatory demo moved into library folder.

---
+++ rltk/rltk.cpp
@@ -0,0 +1,26 @@
+#include "rltk.hpp"
+#include <SFML/Graphics.hpp>
+
+namespace rltk {
+
+void run() {
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+}
+
+}
---
+++ rltk/rltk.hpp
@@ -0,0 +1,8 @@
+/* RLTK (RogueLike Tool Kit) 1.00
+* Copyright (c) 2016-Present, Bracket Productions.
+* Licensed under the LGPL - see LICENSE file.
+*/
+
+namespace rltk {
+void run();
+}
