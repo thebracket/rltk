df76aca00760a1f4b5eb4953b59a71ea84ef4f67
Herbert Wolverson <herberticus@gmail.com>
2016-03-12 11:15:42 -0600

    Placeholder for GUI class.

---
+++ rltk/gui.hpp
@@ -0,0 +1,14 @@
+/* RLTK (RogueLike Tool Kit) 1.00
+ * Copyright (c) 2016-Present, Bracket Productions.
+ * Licensed under the LGPL - see LICENSE file.
+ *
+ * Provides support for complicated GUIs.
+ */
+
+#pragma once
+
+namespace rltk {
+
+
+	
+}
