10e9d2365c2c9b373baa381b30aac7d38f57f252
Herbert Wolverson <herberticus@gmail.com>
2016-03-12 10:42:25 -0600

    Improve box-sweep readability by exposing an offset rather than repeatedly calculating a destination.

--- rltk/visibility.hpp
+++ rltk/visibility.hpp
@@ -14,11 +14,11 @@ namespace rltk {
 namespace visibility_private {
 
 template<class location_t_>
-void internal_2d_sweep(const location_t_ &position, const location_t_ &destination, const int &range, std::function<void(location_t_)> set_visible, 
-	std::function<bool(location_t_)> is_opaque)
+void internal_2d_sweep(const location_t_ &position, const int &range, std::function<void(location_t_)> set_visible, 
+	std::function<bool(location_t_)> is_opaque, const std::pair<int,int> offset)
 {
 	bool blocked = false;
-	line_func(position.x, position.y, destination.x, destination.y, [&blocked, &is_opaque, &set_visible, &range, &position] (int X, int Y) {
+	line_func(position.x, position.y, position.x + offset.first, position.y + offset.second, [&blocked, &is_opaque, &set_visible, &range, &position] (int X, int Y) {
 		float distance = distance2d(position.x, position.y, X, Y);
 		if (distance <= range) {
 			location_t_ pos;
@@ -48,27 +48,10 @@ void visibility_sweep_2d(const location_t_ &position, const int &range, std::fun
 
 	// Box-sweep
 	for (int i=0-range; i<range; ++i) {
-
-		// Going up
-		location_t_ destination;
-		destination.x = position.x + i;
-		destination.y = position.y - range;
-		visibility_private::internal_2d_sweep(position, destination, range, set_visible, is_opaque);
-			
-		// Going down
-		destination.x = position.x + i;
-		destination.y = position.y + range;
-		visibility_private::internal_2d_sweep(position, destination, range, set_visible, is_opaque);
-
-		// Going left
-		destination.x = position.x - range;
-		destination.y = position.y + i;
-		visibility_private::internal_2d_sweep(position, destination, range, set_visible, is_opaque);
-
-		// Going right
-		destination.x = position.x + range;
-		destination.y = position.y + i;
-		visibility_private::internal_2d_sweep(position, destination, range, set_visible, is_opaque);
+		visibility_private::internal_2d_sweep(position, range, set_visible, is_opaque, std::make_pair(i, 0-range));
+		visibility_private::internal_2d_sweep(position, range, set_visible, is_opaque, std::make_pair(i, range));
+		visibility_private::internal_2d_sweep(position, range, set_visible, is_opaque, std::make_pair(0-range, i));
+		visibility_private::internal_2d_sweep(position, range, set_visible, is_opaque, std::make_pair(range, i));
 	}
 }
 
