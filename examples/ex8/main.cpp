3c75b9aaa7d924a1780cc9918075f361215a9548
Herbert Wolverson <herberticus@gmail.com>
2016-03-24 14:27:15 -0500

    Its not retained controls yet, but it looks nice - console window over backdrop.

--- examples/ex8/main.cpp
+++ examples/ex8/main.cpp
@@ -16,6 +16,7 @@ using namespace rltk;
 using namespace rltk::colors;
 
 constexpr int BACKDROP_LAYER = 1;
+constexpr int LOG_LAYER = 2;
 
 void resize_bg(layer_t * l, int w, int h) {
 	// Use the whole window
@@ -29,9 +30,33 @@ void draw_bg(layer_t * l, sf::RenderWindow &window) {
 	window.draw(backdrop);
 }
 
+void resize_log(layer_t * l, int w, int h) {
+	// Simply set the width to the whole window width, and the whole window minus 16 pixels (for the heading)
+	l->w = w - 160;
+	l->h = h - 32;
+
+	// If the log window would take up the whole screen, hide it
+	if (l->w < 0) {
+		l->console->visible = false;
+	} else {
+		l->console->visible = true;
+	}
+	l->x = w - 160;
+}
+
 // Tick is called every frame. The parameter specifies how many ms have elapsed
 // since the last time it was called.
 void tick(double duration_ms) {
+	term(LOG_LAYER)->clear(vchar{' ', WHITE, DARKEST_GREEN});
+	term(LOG_LAYER)->box(DARKEST_GREEN, BLACK);
+	term(LOG_LAYER)->print(1,1, "Log Entry", LIGHT_GREEN, DARKEST_GREEN);
+	term(LOG_LAYER)->print(1,2, "More text!", LIGHT_GREEN, DARKEST_GREEN);
+	term(LOG_LAYER)->print(1,3, "Even more...", LIGHT_GREEN, DARKEST_GREEN);
+	term(LOG_LAYER)->print(1,4, "... goes here", LIGHT_GREEN, DARKEST_GREEN);
+
+	std::stringstream ss;
+	ss << std::setiosflags(std::ios::fixed) << std::setprecision(0) << (1000.0/duration_ms) << " FPS";
+	term(LOG_LAYER)->print(1,6, ss.str(), WHITE, DARKEST_GREEN);
 }
 
 // Your main function
@@ -48,6 +73,8 @@ int main()
 	// Now we add an owner-draw background layer. "Owner-draw" means that it the library will ask it to
 	// draw itself with a call-back function.
 	gui->add_owner_layer(BACKDROP_LAYER, 0, 0, 1024, 768, resize_bg, draw_bg);
+	gui->add_layer(LOG_LAYER, 864, 32, 160, 768-32, "8x16", resize_log);
+	term(LOG_LAYER)->set_alpha(196); // Make the overlay translucent
 
 	// Main loop - calls the 'tick' function you defined for each frame.
 	run(tick);
