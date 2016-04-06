79f1e4d7eecbef3620f0eac51367aad11dca751b
Herbert Wolverson <herbert@iMac.local>
2016-04-06 14:35:30 -0500

    Example 10 now shows a very simple @ controlled by the keyboard, running around a random map. It uses the entity component system.

--- examples/ex10/main.cpp
+++ examples/ex10/main.cpp
@@ -10,29 +10,103 @@
 
 // We're using a stringstream to build the hello world message.
 #include <sstream>
+#include <algorithm>
 
 // For convenience, import the whole rltk namespace. You may not want to do this
 // in larger projects, to avoid naming collisions.
 using namespace rltk;
 using namespace rltk::colors;
+using std::size_t;
+
+constexpr int map_width = 100;
+constexpr int map_height = 100;
+constexpr int map_size = map_width * map_height;
+int map_idx(const int x, const int y) { return (y * map_width) + x; }
+std::vector<int> map_tiles;
+random_number_generator rng;
+
+size_t player_id;
+
+struct position { int x, y; };
+struct renderable { int glyph; color_t fg=colors::WHITE; color_t bg=colors::BLACK; };
+
+// Clipping info
+int left_x, right_x, top_y, bottom_y;
+
+struct camera_system : public base_system {
+	virtual void configure() override {
+		// Create the player
+		auto player = create_entity()
+			->assign(position{map_width/2, map_height/2})
+			->assign(renderable{'@', colors::YELLOW});
+		player_id = player->id;
+	}
+
+	virtual void update(const double duration_ms) override {
+		// In this case, we just want to print "Hello World" in white on black.
+		if (console->dirty) {
+			console->clear();
+
+			// Find the camera
+			position * camera_loc = entity(player_id)->component<position>();
+			left_x = camera_loc->x - (console->term_width / 2);
+			right_x = camera_loc->x + (console->term_width / 2);
+			top_y = camera_loc->y - (console->term_height/2);
+			bottom_y = camera_loc->y + (console->term_height/2)+1;
+
+			for (int y=top_y; y<bottom_y; ++y) {
+				for (int x=left_x; x<right_x; ++x) {
+					if (x >= 0 && x < map_width && y >= 0 && y < map_height) {
+						vchar map_char{'.', colors::DARK_GREY, colors::BLACK};
+						const int map_index = map_idx(x,y);
+						switch (map_tiles[map_index]) {
+							case 0 : map_char.glyph = '.'; break;
+							case 1 : map_char.glyph = '#'; break;
+							default : map_char.glyph = 'E'; // This shouldn't happen
+						}
+						console->set_char(x-left_x, y-top_y, map_char);
+					}
+				}
+			}
+		}		
+	}
+};
+
+struct actor_render_system : public base_system {
+	virtual void update(const double duration_ms) override {
+		each<position, renderable>([] (entity_t &entity, position &pos, renderable &render) {
+			console->set_char(pos.x-left_x, pos.y-top_y, vchar{ render.glyph, render.fg, render.bg });
+		});
+	}
+};
+
+struct player_system : public base_system {
+	virtual void update(const double duration_ms) override {
+		position * camera_loc = entity(player_id)->component<position>();
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left) && map_tiles[map_idx(camera_loc->x-1, camera_loc->y)]==0 ) {
+			if (camera_loc->x > 0) camera_loc->x--;
+			console->dirty = true; // To be replaced with a message when we have them
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right) && map_tiles[map_idx(camera_loc->x+1, camera_loc->y)]==0 ) {
+			if (camera_loc->x < map_width) camera_loc->x++;
+			console->dirty = true;
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up) && map_tiles[map_idx(camera_loc->x, camera_loc->y-1)]==0 ) {
+			if (camera_loc->y > 0) camera_loc->y--;
+			console->dirty = true;
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down) && map_tiles[map_idx(camera_loc->x, camera_loc->y+1)]==0 ) {
+			if (camera_loc->y < map_width) camera_loc->y++;
+			console->dirty = true;
+		}
+	}
+};
 
 // Tick is called every frame. The parameter specifies how many ms have elapsed
 // since the last time it was called.
 void tick(double duration_ms) {
-	// In this case, we just want to print "Hello World" in white on black.
-	if (console->dirty) {
-		console->clear();
-		console->print(1,1,"Hello World", WHITE, BLACK);
-	}
+	ecs_tick(duration_ms);
 }
-
-struct position_component {
-	int x, y;
-};
-
-struct position_component2 {
-	int x, y;
-};
 
 // Your main function
 int main()
@@ -45,44 +119,33 @@ int main()
 	// The fourth parameter is the window title.
 	// The final parameter says that we'd like the default console to use an 8x16 VGA font. Not so great for games, but easy to read!
 	init(config_simple("../assets", 80, 25, "RLTK Hello World", "8x16"));
-
-	auto entity1 = create_entity()
-		->assign(position_component{1,1})
-		->assign(position_component2{2,2});
-
-	auto entity2 = create_entity()
-		->assign(position_component{3,3});
-
-	each([] (entity_t &e) { std::cout << "Hi, I am Entity #" << e.id << "\n"; });
 	
-	each<position_component>([] (entity_t &e, position_component &p) { 
-		std::cout << "Hi, I am Entity #" << e.id << " and I have a position component (" << p.x << "/" << p.y << ")!\n"; 
-	});
+	// Zero the map other than the edges
+	map_tiles.resize(map_size);
+	std::fill(map_tiles.begin(), map_tiles.end(), 0);
+	for (int i=0; i<map_width; ++i) {
+		map_tiles[map_idx(i, 0)] = 1;
+		map_tiles[map_idx(i, map_height-1)] = 1;
+	}
+	for (int i=0; i<map_width; ++i) {
+		map_tiles[map_idx(0, i)] = 1;
+		map_tiles[map_idx(map_width-1, i)] = 1;
+	}
+	// Random debris
+	for (int y=1; y<map_height-1; ++y) {
+		for (int x=1; x<map_width-1; ++x) {
+			if (rng.roll_dice(1,4)==1 && (x!=map_width/2 || y!=map_height/2)) map_tiles[map_idx(x,y)]=1;
+		}
+	}
 
-	// Quick test of the helper functions
-	entity(1)->component<position_component>()->x = 10;
+	// Create our systems
+	add_system(std::make_unique<player_system>());
+	add_system(std::make_unique<camera_system>());
+	add_system(std::make_unique<actor_render_system>());
 
-	each<position_component, position_component2>([] (entity_t &e, position_component &p, position_component2 &p2) {
-		std::cout << "Hi< I am Entity #" << e.id << " and I have two types of position component.\n";
-		std::cout << "   - Position 1: " << p.x << "/" << p.y << "\n";
-		std::cout << "   - Position 2: " << p2.x << "/" << p2.y << "\n";
-	});
-
-	all_components<position_component>([] (entity_t &e, position_component &p) {
-		std::cout << "Hi, I'm position_component at " << p.x << "/" << p.y << ", and I belong to Entity #" << e.id << "\n";
-	});	
-	delete_component<position_component>(1);
-	delete_component<position_component>(2, true);
-	ecs_garbage_collect();
-	all_components<position_component>([] (entity_t &e, position_component &p) {
-		std::cout << "Hi, I'm am error! position_component at " << p.x << "/" << p.y << ", and I belong to Entity #" << e.id << "\n";
-	});	
-	each([] (entity_t &e) { 
-		std::cout << "Hi, I am Surviving Entity #" << e.id << "!\n"; 
-	});
-	
 	// Enter the main loop. "tick" is the function we wrote above.
-	//run(tick);
+	ecs_configure();
+	run(tick);
 
     return 0;
 }
