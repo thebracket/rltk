1e52fe3f5bea5f6fb9da6dbde0d88f89ea728e75
Herbert Wolverson <herberticus@gmail.com>
2016-04-22 15:23:56 -0500

    Perlin noise wasn't actually Perlin; oops. It is now.

--- rltk/perlin_noise.cpp
+++ rltk/perlin_noise.cpp
@@ -1,122 +1,92 @@
 #include "perlin_noise.hpp"
+#include <iostream>
+#include <cmath>
+#include <random>
+#include <algorithm>
 
 namespace rltk {
 
-perlin_noise::perlin_noise()
-{
-	persistence_d = 0;
-	frequency_d = 0;
-	amplitude_d = 0;
-	octaves_i = 0;
-	random_seed_i = 0;
+// Initialize with the reference values for the permutation vector
+perlin_noise::perlin_noise() {
+	
+	// Initialize the permutation vector with the reference values
+	p = {
+		151,160,137,91,90,15,131,13,201,95,96,53,194,233,7,225,140,36,103,30,69,142,
+		8,99,37,240,21,10,23,190, 6,148,247,120,234,75,0,26,197,62,94,252,219,203,117,
+		35,11,32,57,177,33,88,237,149,56,87,174,20,125,136,171,168, 68,175,74,165,71,
+		134,139,48,27,166,77,146,158,231,83,111,229,122,60,211,133,230,220,105,92,41,
+		55,46,245,40,244,102,143,54, 65,25,63,161,1,216,80,73,209,76,132,187,208, 89,
+		18,169,200,196,135,130,116,188,159,86,164,100,109,198,173,186, 3,64,52,217,226,
+		250,124,123,5,202,38,147,118,126,255,82,85,212,207,206,59,227,47,16,58,17,182,
+		189,28,42,223,183,170,213,119,248,152, 2,44,154,163, 70,221,153,101,155,167, 
+		43,172,9,129,22,39,253, 19,98,108,110,79,113,224,232,178,185, 112,104,218,246,
+		97,228,251,34,242,193,238,210,144,12,191,179,162,241, 81,51,145,235,249,14,239,
+		107,49,192,214, 31,181,199,106,157,184, 84,204,176,115,121,50,45,127, 4,150,254,
+		138,236,205,93,222,114,67,29,24,72,243,141,128,195,78,66,215,61,156,180 };
+	// Duplicate the permutation vector
+	p.insert(p.end(), p.begin(), p.end());
 }
 
-perlin_noise::perlin_noise(double _persistence, double _frequency,
-		double _amplitude, int _octaves, int _randomseed)
-{
-	persistence_d = _persistence;
-	frequency_d = _frequency;
-	amplitude_d = _amplitude;
-	octaves_i = _octaves;
-	random_seed_i = 2 + _randomseed * _randomseed;
+// Generate a new permutation vector based on the value of seed
+perlin_noise::perlin_noise(unsigned int seed) {
+	p.resize(256);
+
+	// Fill p with values from 0 to 255
+	std::iota(p.begin(), p.end(), 0);
+
+	// Initialize a random engine with seed
+	std::default_random_engine engine(seed);
+
+	// Suffle  using the above random engine
+	std::shuffle(p.begin(), p.end(), engine);
+
+	// Duplicate the permutation vector
+	p.insert(p.end(), p.begin(), p.end());
 }
 
-void perlin_noise::set(double _persistence, double _frequency,
-		double _amplitude, int _octaves, int _randomseed)
-{
-	persistence_d = _persistence;
-	frequency_d = _frequency;
-	amplitude_d = _amplitude;
-	octaves_i = _octaves;
-	random_seed_i = 2 + _randomseed * _randomseed;
+double perlin_noise::noise(double x, double y, double z) {
+	// Find the unit cube that contains the point
+	int X = (int) floor(x) & 255;
+	int Y = (int) floor(y) & 255;
+	int Z = (int) floor(z) & 255;
+
+	// Find relative x, y,z of point in cube
+	x -= floor(x);
+	y -= floor(y);
+	z -= floor(z);
+
+	// Compute fade curves for each of x, y, z
+	double u = fade(x);
+	double v = fade(y);
+	double w = fade(z);
+
+	// Hash coordinates of the 8 cube corners
+	int A = p[X] + Y;
+	int AA = p[A] + Z;
+	int AB = p[A + 1] + Z;
+	int B = p[X + 1] + Y;
+	int BA = p[B] + Z;
+	int BB = p[B + 1] + Z;
+
+	// Add blended results from 8 corners of cube
+	double res = lerp(w, lerp(v, lerp(u, grad(p[AA], x, y, z), grad(p[BA], x-1, y, z)), lerp(u, grad(p[AB], x, y-1, z), grad(p[BB], x-1, y-1, z))),	lerp(v, lerp(u, grad(p[AA+1], x, y, z-1), grad(p[BA+1], x-1, y, z-1)), lerp(u, grad(p[AB+1], x, y-1, z-1),	grad(p[BB+1], x-1, y-1, z-1))));
+	return (res + 1.0)/2.0;
 }
 
-double perlin_noise::get_height(double x, double y) const
-{
-	return amplitude_d * total(x, y);
+double perlin_noise::fade(double t) { 
+	return t * t * t * (t * (t * 6 - 15) + 10);
 }
 
-double perlin_noise::total(double i, double j) const
-{
-	//properties of one octave (changing each loop)
-	double t = 0.0f;
-	double _amplitude = 1;
-	double freq = frequency_d;
-
-	for (int k = 0; k < octaves_i; k++)
-	{
-		t += get_value(j * freq + random_seed_i, i * freq + random_seed_i)
-				* _amplitude;
-		_amplitude *= persistence_d;
-		freq *= 2;
-	}
-
-	return t;
+double perlin_noise::lerp(double t, double a, double b) { 
+	return a + t * (b - a); 
 }
 
-double perlin_noise::get_value(double x, double y) const
-{
-	int Xint = (int) x;
-	int Yint = (int) y;
-	double Xfrac = x - Xint;
-	double Yfrac = y - Yint;
-
-	//noise values
-	double n01 = noise(Xint - 1, Yint - 1);
-	double n02 = noise(Xint + 1, Yint - 1);
-	double n03 = noise(Xint - 1, Yint + 1);
-	double n04 = noise(Xint + 1, Yint + 1);
-	double n05 = noise(Xint - 1, Yint);
-	double n06 = noise(Xint + 1, Yint);
-	double n07 = noise(Xint, Yint - 1);
-	double n08 = noise(Xint, Yint + 1);
-	double n09 = noise(Xint, Yint);
-
-	double n12 = noise(Xint + 2, Yint - 1);
-	double n14 = noise(Xint + 2, Yint + 1);
-	double n16 = noise(Xint + 2, Yint);
-
-	double n23 = noise(Xint - 1, Yint + 2);
-	double n24 = noise(Xint + 1, Yint + 2);
-	double n28 = noise(Xint, Yint + 2);
-
-	double n34 = noise(Xint + 2, Yint + 2);
-
-	//find the noise values of the four corners
-	double x0y0 = 0.0625 * (n01 + n02 + n03 + n04)
-			+ 0.125 * (n05 + n06 + n07 + n08) + 0.25 * (n09);
-	double x1y0 = 0.0625 * (n07 + n12 + n08 + n14)
-			+ 0.125 * (n09 + n16 + n02 + n04) + 0.25 * (n06);
-	double x0y1 = 0.0625 * (n05 + n06 + n23 + n24)
-			+ 0.125 * (n03 + n04 + n09 + n28) + 0.25 * (n08);
-	double x1y1 = 0.0625 * (n09 + n16 + n28 + n34)
-			+ 0.125 * (n08 + n14 + n06 + n24) + 0.25 * (n04);
-
-	//interpolate between those values according to the x and y fractions
-	double v1 = interpolate(x0y0, x1y0, Xfrac); //interpolate in x direction (y)
-	double v2 = interpolate(x0y1, x1y1, Xfrac); //interpolate in x direction (y+1)
-	double fin = interpolate(v1, v2, Yfrac); //interpolate in y direction
-
-	return fin;
-}
-
-double perlin_noise::interpolate(double x, double y, double a) const
-{
-	double negA = 1.0 - a;
-	double negASqr = negA * negA;
-	double fac1 = 3.0 * (negASqr) - 2.0 * (negASqr * negA);
-	double aSqr = a * a;
-	double fac2 = 3.0 * aSqr - 2.0 * (aSqr * a);
-
-	return x * fac1 + y * fac2; //add the weighted factors
-}
-
-double perlin_noise::noise(int x, int y) const
-{
-	int n = x + y * 57;
-	n = (n << 13) ^ n;
-	int t = (n * (n * n * 15731 + 789221) + 1376312589) & 0x7fffffff;
-	return 1.0 - double(t) * 0.931322574615478515625e-9; /// 1073741824.0);
+double perlin_noise::grad(int hash, double x, double y, double z) {
+	int h = hash & 15;
+	// Convert lower 4 bits of hash inot 12 gradient directions
+	double u = h < 8 ? x : y,
+		   v = h < 4 ? y : h == 12 || h == 14 ? x : z;
+	return ((h & 1) == 0 ? u : -u) + ((h & 2) == 0 ? v : -v);
 }
 
 }
--- rltk/perlin_noise.hpp
+++ rltk/perlin_noise.hpp
@@ -1,75 +1,27 @@
+// THIS CLASS IS A TRANSLATION TO C++11 FROM THE REFERENCE
+// JAVA IMPLEMENTATION OF THE IMPROVED PERLIN FUNCTION (see http://mrl.nyu.edu/~perlin/noise/)
+// THE ORIGINAL JAVA IMPLEMENTATION IS COPYRIGHT 2002 KEN PERLIN
+
 #pragma once
+
+#include <vector>
 
 namespace rltk {
 
-class perlin_noise
-{
+class perlin_noise {
+	// The permutation vector
+	std::vector<int> p;
 public:
-
-	// Constructor
+	// Initialize with the reference values for the permutation vector
 	perlin_noise();
-	perlin_noise(double _persistence, double _frequency, double _amplitude,
-			int _octaves, int _randomseed);
-
-	// Get Height
-	double get_height(double x, double y) const;
-
-	// Get
-	double persistence() const
-	{
-		return persistence_d;
-	}
-	double frequency() const
-	{
-		return frequency_d;
-	}
-	double amplitude() const
-	{
-		return amplitude_d;
-	}
-	int octaves() const
-	{
-		return octaves_i;
-	}
-	int random_seed() const
-	{
-		return random_seed_i;
-	}
-
-	// Set
-	void set(double _persistence, double _frequency, double _amplitude,
-			int _octaves, int _randomseed);
-
-	void set_persistence(double _persistence)
-	{
-		persistence_d = _persistence;
-	}
-	void set_frequency(double _frequency)
-	{
-		frequency_d = _frequency;
-	}
-	void set_amplitude(double _amplitude)
-	{
-		amplitude_d = _amplitude;
-	}
-	void set_octaves(int _octaves)
-	{
-		octaves_i = _octaves;
-	}
-	void set_random_seed(int _randomseed)
-	{
-		random_seed_i = _randomseed;
-	}
-
+	// Generate a new permutation vector based on the value of seed
+	perlin_noise(unsigned int seed);
+	// Get a noise value, for 2D images z can have any value
+	double noise(double x, double y, double z);
 private:
-
-	double total(double i, double j) const;
-	double get_value(double x, double y) const;
-	double interpolate(double x, double y, double a) const;
-	double noise(int x, int y) const;
-
-	double persistence_d, frequency_d, amplitude_d;
-	int octaves_i, random_seed_i;
+	double fade(double t);
+	double lerp(double t, double a, double b);
+	double grad(int hash, double x, double y, double z);
 };
 
 }
