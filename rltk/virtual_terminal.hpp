9e262453b94129e82de86c4371b0771270f0d78c
Herbert Wolverson <herbert@iMac.local>
2016-05-31 09:23:26 -0500

    Add noexcept to some functions.

--- rltk/virtual_terminal.hpp
+++ rltk/virtual_terminal.hpp
@@ -45,56 +45,56 @@ public:
 	/*
 	 * Resize the terminal to match width x height pixels.
 	 */
-	void resize_pixels(const int width, const int height);
+	void resize_pixels(const int width, const int height) noexcept;
 
 	/*
 	 * Resize the terminal to match width x height virtual characters.
 	 */
-	void resize_chars(const int width, const int height);
+	void resize_chars(const int width, const int height) noexcept;
 
 	/*
 	 * Clears the virtual terminal to black spaces.
 	 */
-	void clear();
+	void clear() noexcept;
 
 	/*
 	 * Clears the virtual terminal to a user-provided character.
 	 * vchar; the character to which the terminal should be set.
 	 */
-	void clear(const vchar &target);
+	void clear(const vchar &target) noexcept;
 
 	/*
 	 * Helper function that returns the index of a character location in the backing vector.
 	 */
-	inline int at(const int x, const int y) { return ( y * term_width) + x; }
+	inline int at(const int x, const int y) noexcept { return ( y * term_width) + x; }
 
 	/*
 	 * Set a character at backing-vector idx (use "at" to calculate) to the specified target virtual
 	 * character.
 	 */
-	void set_char(const int idx, const vchar &target);
+	void set_char(const int idx, const vchar &target) noexcept;
 
 	/*
 	 * Set a character at x/y to the specified target virtual character.
 	 */
-	inline void set_char(const int x, const int y, vchar target) { set_char(at(x,y), target); }
+	inline void set_char(const int x, const int y, vchar target) noexcept { set_char(at(x,y), target); }
 
 	/*
 	 * Print a string to the terminal, at location x/y, string s, with foreground and background of fg and bg.
 	 * If you don't specify colors, it will do white on black.
 	 */
-	void print(const int x, const int y, const std::string &s, const color_t &fg = colors::WHITE, const color_t &bg = colors::BLACK);
+	void print(const int x, const int y, const std::string &s, const color_t &fg = colors::WHITE, const color_t &bg = colors::BLACK) noexcept;
 
 	/*
 	 * Center a string (relative to the terminal size), and print it at location y.
 	 */
-	void print_center(const int y, const std::string &s, const color_t &fg = colors::WHITE, const color_t &bg = colors::BLACK);
+	void print_center(const int y, const std::string &s, const color_t &fg = colors::WHITE, const color_t &bg = colors::BLACK) noexcept;
 
 	/*
 	 * Draw a box taht encompasses the whole terminal boundary, in color fg/bg. If double_lines is set to true, then
 	 * it will use the two-line/thick box characters.
 	 */
-	inline void box(const color_t &fg = colors::WHITE, const color_t &bg = colors::BLACK, bool double_lines=false) {
+	inline void box(const color_t &fg = colors::WHITE, const color_t &bg = colors::BLACK, bool double_lines=false) noexcept {
 		box (0, 0, term_width-1, term_height-1, fg, bg, double_lines);
 	}
 
@@ -102,33 +102,33 @@ public:
 	 * Draw a box at x/y of size w/h, in color fg/bg (or black/white if not specified). If double_lines is true,
 	 * it will use the two-line/thick box characters.
 	 */
-	void box(const int x, const int y, const int w, const int h, const color_t &fg = colors::WHITE, const color_t &bg = colors::BLACK, bool double_lines=false);
+	void box(const int x, const int y, const int w, const int h, const color_t &fg = colors::WHITE, const color_t &bg = colors::BLACK, bool double_lines=false) noexcept;
 
 	/*
 	 * Renders the terminal to the specified renderable. Don't call this directly - the toolkit will take care of it.
 	 */
-	void render(sf::RenderWindow &window);
+	void render(sf::RenderWindow &window) noexcept;
 
 	/*
 	 * Sets the global translucency level for the console. You can use this to make a translucent console layer on top of
 	 * other items.
 	 */
-	inline void set_alpha(const uint8_t new_alpha) { alpha = new_alpha; }
+	inline void set_alpha(const uint8_t new_alpha) noexcept { alpha = new_alpha; }
 
 	/*
 	 * Use this to tint the entire rendering of the console.
 	 */
-	inline void set_tint(const color_t new_tint) { tint = new_tint; }
+	inline void set_tint(const color_t new_tint) noexcept { tint = new_tint; }
 
 	/*
 	 * Use this to move the terminal in x/y screen pixels.
 	 */
-	inline void set_offset(int x, int y) { offset_x = x; offset_y = y; };
+	inline void set_offset(int x, int y) noexcept { offset_x = x; offset_y = y; };
 
 	/*
 	 * This gets the current font size in pixels, first is width, second is height.
 	 */
-	inline std::pair<int,int> get_font_size() { return font->character_size; }
+	inline std::pair<int,int> get_font_size() noexcept { return font->character_size; }
 
 	int term_width;
 	int term_height;
